698333dc9ad3377ff606c73182d1d3d95942d94e
Thorstyx <145431159+Thorstyx@users.noreply.github.com>
2023-12-26 16:31:43 +0100

    Add files via upload

---
+++ Site pour de vrai de vrai/darkmodetest/script.js
@@ -0,0 +1,22 @@
+let button = document.querySelector('button');
+let type = 'light';
+
+let headStyle = (type) => {
+  let head = document.getElementsByTagName('head')[0];
+  let style = document.createElement('link');
+  style.rel = 'stylesheet';
+  style.id = 'colorTheme';
+  style.href = `./style.${type}.css`;
+  head.appendChild(style);
+  document.documentElement.setAttribute('data-theme', type)
+  console.log(style);
+}
+
+button.addEventListener('click', () => {
+  let colorTheme = document.querySelector('#colorTheme');
+  colorTheme.parentNode.removeChild(colorTheme);
+  let typeTheme = document.documentElement.getAttribute('data-theme');
+  typeTheme === 'light' ? headStyle('dark') : headStyle('light')
+})
+
+window.addEventListener('load', headStyle(type));
